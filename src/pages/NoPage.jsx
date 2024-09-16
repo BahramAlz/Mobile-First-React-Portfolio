11528c57e36c632cd53d7694b3eac82944486e81
bahram alzuhairi <bahram.alzuhairi@hyperisland.se>
2024-09-16 23:43:28 +0200

    No page adjustments

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const NoPage = () => {
-  return <h1>404</h1>;
+  return (
+    <div className="py-10 px-6 dark:bg-[#fff] flex flex-col gap-16">
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-1">
+          <h2 className="text-2xl text-white font-bold dark:text-black">
+            Error 404
+          </h2>
+          <p className="text-[#b4b4b4] dark:text-gray-700">
+            This page doesn't exist.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NoPage;
